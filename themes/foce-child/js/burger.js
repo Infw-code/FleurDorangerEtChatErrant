dceb3dfc36e552c558e1cabd95bced552f9fed64
Romain Richez <romain.richez77@gmail.com>
2025-07-15 03:18:53 +0200

    Mise à jour du contenu

--- themes/foce-child/js/burger.js
+++ themes/foce-child/js/burger.js
@@ -4,7 +4,6 @@ document.addEventListener('DOMContentLoaded', () => {
   const mainNav = document.querySelector('.main-navigation');
 
   menuToggle.addEventListener('click', () => {
-    // Toggle classes existantes
     menuToggle.classList.toggle('active');      
     mainNav.classList.toggle('toggled');         
     if (menuToggle.classList.contains('active')){
@@ -13,10 +12,6 @@ document.addEventListener('DOMContentLoaded', () => {
     else {
       mainNav.classList.remove('toggled');
     }
-    // Toggle classe sur <body> pour bloquer le bouton
-    /*document.body.classList.toggle('menu-open');*/
-
-    // Mettre à jour aria-expanded
     const expanded = menuToggle.getAttribute('aria-expanded') === 'true';
     menuToggle.setAttribute('aria-expanded', String(!expanded));
   });
